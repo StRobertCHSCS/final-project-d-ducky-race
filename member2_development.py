2eb4a9de53dd25b7635643cf67b450a59804ccf1
SJ7270 <48096906+SJ7270@users.noreply.github.com>
2020-01-05 17:40:45 -0500

    making the subtraction problems update

--- member2_development.py
+++ member2_development.py
@@ -22,6 +22,7 @@ def update(delta_time):
     pass
 
 def on_draw():
+    global duck_x, duck_y, jump, counter, difference, first_int, second_int
     arcade.start_render()
     if current_screen == "menu":
         arcade.draw_text("D DUCKY RACE", WIDTH/2 - 200, HEIGHT/2, arcade.color.WHITE, 30)
@@ -33,6 +34,18 @@ def on_draw():
         arcade.draw_circle_filled(x+15, y+45, 3, arcade.color.BLACK)
         arcade.draw_rectangle_filled(x-10,y-32, 3, 15, arcade.color.BROWN)
         arcade.draw_rectangle_filled(x+10,y-32, 3, 15, arcade.color.BROWN)
+        counter += 1
+        if counter >= 10 and counter <= 30:
+            if first_int > second_int:
+                difference = first_int-second_int
+                arcade.draw_text(str(first_int)+"-"+str(second_int), 200, 400, arcade.color.BLACK, 25)
+            if second_int > first_int:
+                difference = second_int-first_int
+                arcade.draw_text(str(second_int)+"-"+str(first_int), 200, 400, arcade.color.BLACK, 25)
+        if counter == 30:
+            counter = 0
+            first_int = random.randint(1, 9)
+            second_int = random.randint(1, 9)
 
 def on_key_press(key, modifiers):
     global current_screen
@@ -76,17 +89,6 @@ def setup():
     window.on_mouse_press = on_mouse_press
 
     arcade.run()
-
-
-def draw_menu():
-    pass
-
-
-def draw_high_score():
-    pass
-
-def draw_timer():
-    pass
 
 
 
