a5facb5836ebfd1bec6c9777eb0ebe281ac5b4bb
SJ7270 <seungjoo.oh21@ycdsbk12.ca>
2020-01-06 14:31:37 -0500

    jumping straight up

--- member2_development.py
+++ member2_development.py
@@ -1,87 +1,140 @@
+"""
+--------------------------------------------------------------------------------------------------------------------
+Name:   member1_development.py
+Purpose:    the development of the first member
+
+Author: LMo
+Created:
+--------------------------------------------------------------------------------------------------------------------
+"""
 import arcade
 import random
 
-
-
 WIDTH = 640
 HEIGHT = 480
-x = 25
-y = 50
+
+
+duck_x = 25
+duck_y = 50
 jump = False
-counter = 0
+started = False
 died = False
 difference = 0
+counter = 0
 first_int = random.randint(1, 9)
 second_int = random.randint(1, 9)
 
-current_screen = "menu"
 
-
-
-def update(delta_time):
+def on_update(delta_time):
     pass
 
+def draw_duck(x, y):
+    arcade.draw_circle_filled(x, y, 25, arcade.color.YELLOW)
+    arcade.draw_circle_filled(x+10, y+38, 15, arcade.color.YELLOW)
+    arcade.draw_triangle_filled(x+24, y+40, x+24, y+35, x+30, y+37.5, arcade.color.DARK_RED)
+    arcade.draw_circle_filled(x+15, y+45, 3, arcade.color.BLACK)
+    arcade.draw_rectangle_filled(x-10,y-32, 3, 15, arcade.color.BROWN)
+    arcade.draw_rectangle_filled(x+10,y-32, 3, 15, arcade.color.BROWN)
+
+def draw_title():
+    arcade.draw_text("D DUCKY RACE", 50, 500, arcade.color.BLACK, 30)
+   
+
 def on_draw():
-    global duck_x, duck_y, jump, counter, difference, first_int, second_int
+    global duck_x
+    global duck_y
+    global jump
+    global difference
+    global counter
+    global first_int
+    global second_int
+
     arcade.start_render()
-    if current_screen == "menu":
-        arcade.draw_text("D DUCKY RACE", WIDTH/2 - 200, HEIGHT/2, arcade.color.WHITE, 30)
-        arcade.draw_text('Press Space to Play', WIDTH/2 - 200, HEIGHT/2 - 50, arcade.color.WHITE, 30)
-    if current_screen == "start":
-        arcade.draw_circle_filled(x, y, 25, arcade.color.YELLOW)
-        arcade.draw_circle_filled(x+10, y+38, 15, arcade.color.YELLOW)
-        arcade.draw_triangle_filled(x+24, y+40, x+24, y+35, x+30, y+37.5, arcade.color.DARK_RED)
-        arcade.draw_circle_filled(x+15, y+45, 3, arcade.color.BLACK)
-        arcade.draw_rectangle_filled(x-10,y-32, 3, 15, arcade.color.BROWN)
-        arcade.draw_rectangle_filled(x+10,y-32, 3, 15, arcade.color.BROWN)
+    score = 0
+
+    #does random subtraction problems (1 digit only)
+    if started == False and died == False:
+        arcade.set_background_color(arcade.color.PINK_PEARL)
+        arcade.draw_text("WELCOME TO D DUCKY RACE!\nPress Any Key to Start!", 100, 300, arcade.color.BLACK, 30)
+    if started == True and died == False:
+        arcade.set_background_color(arcade.color.LIGHT_BLUE)
         counter += 1
-        if counter >= 10 and counter <= 30:
+        draw_title()
+        if 10<=counter<40:
             if first_int > second_int:
                 difference = first_int-second_int
-                arcade.draw_text(str(first_int)+"-"+str(second_int), 200, 400, arcade.color.BLACK, 25)
+                arcade.draw_text(str(first_int)+"-"+str(second_int), 300, 400, arcade.color.BLACK, 25)
             if second_int > first_int:
                 difference = second_int-first_int
-                arcade.draw_text(str(second_int)+"-"+str(first_int), 200, 400, arcade.color.BLACK, 25)
-        if counter == 30:
+                arcade.draw_text(str(second_int)+"-"+str(first_int), 300, 400, arcade.color.BLACK, 25)
+        if counter == 60:
             counter = 0
             first_int = random.randint(1, 9)
             second_int = random.randint(1, 9)
+        
+        #makes the duck jump
+        draw_duck(duck_x, duck_y)
+        if jump == True and duck_y<=160: 
+            duck_y += 5
+        elif duck_y >= 160:
+            duck_y = duck_y * -1
+    
+    if died == True and started == True:
+        arcade.set_background_color(arcade.color.BLACK)
+
+        dead_emoji = arcade.load_texture("Dead emoji.jpg")
+        arcade.draw_texture_rectangle(300, 370, 200, 100, dead_emoji, 0)
+
+        arcade.draw_text("You have died.\nYour final score is "+str(score), 100, 200, arcade.color.WHITE, 50)
+
+        arcade.draw_rectangle_filled(300, 130, 150, 100, arcade.color.PINK_LAVENDER)
+        arcade.draw_text("PLAY AGAIN", 255, 123, arcade.color.BLACK, 15)
+
 
 def on_key_press(key, modifiers):
-    global current_screen
-    if current_screen == "menu":
-        if key == arcade.key.SPACE:
-            current_screen = "start"
-
-    elif current_screen == "start":
-        if key == arcade.key.ESCAPE:
-                current_screen = "menu"
-
-
-    elif current_screen == "high score":
-        if key == arcade.key.ESCAPE:
-            current_screen = "menu"
-
-    elif current_screen == "timer":
-        if key == arcade.key.ESCAPE:
-            current_screen = "menu"
-        elif key == arcade.key.SPACE:
-            timing = False
-
+    global jump
+    global difference
+    global started
+    if started == True and key == arcade.key.SPACE:
+        started = False
+    if started == False and died == False:
+        started = True
+    if started == True and died == False:
+        if difference == 0 and key == arcade.key.KEY_0:
+            jump = True
+        if difference == 1 and key == arcade.key.KEY_1:
+            jump = True
+        if difference == 2 and key == arcade.key.KEY_2:
+            jump = True
+        if difference == 3 and key == arcade.key.KEY_3:
+            jump = True
+        if difference == 4 and key == arcade.key.KEY_4:
+            jump = True
+        if difference == 5 and key == arcade.key.KEY_5:
+            jump = True
+        if difference == 6 and key == arcade.key.KEY_6:
+            jump = True
+        if difference == 7 and key == arcade.key.KEY_7:
+            jump = True
+        if difference == 8 and key == arcade.key.KEY_8:
+            jump = True
+    
 
 def on_key_release(key, modifiers):
     pass
 
 
 def on_mouse_press(x, y, button, modifiers):
-    pass
+    global died
+    if 235<x<385 and 80<y<180 and died == True:
+        died = False
 
 
 def setup():
     arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
-    arcade.set_background_color(arcade.color.BANGLADESH_GREEN)
-    arcade.schedule(update, 1/60)
+    arcade.schedule(on_update, 1/60)
 
+    # Override arcade window methods
     window = arcade.get_window()
     window.on_draw = on_draw
     window.on_key_press = on_key_press
@@ -89,8 +142,6 @@ def setup():
     window.on_mouse_press = on_mouse_press
 
     arcade.run()
-
-
 
 
 if __name__ == '__main__':
